7f4b771664347ed9d97a0a2b94a7d1762a65e5df
Jakub Kozłowski <kubukoz@gmail.com>
2016-01-09 20:03:11 +0100

    Dzialajace wspolczynniki dla f. bazowych od sin(x)

--- test/gen.c
+++ test/gen.c
@@ -3,27 +3,27 @@
 #include <math.h>
 #include <time.h>
 
-double fun( double x ) {
-//				double r= ((double)rand() - RAND_MAX/2.0)/RAND_MAX/5; // +-10%
+double fun(double x) {
+    double r = ((double) rand() - RAND_MAX / 2.0) / RAND_MAX / 5; // +-10%
 //				return (1+r)*(10*x*x*x*x+5*x*x*x+x*x);
-	return sin(x);
+    return (1+r)*(2*sin(x) + 3*cos(x) - 2 * sin(2*x) + 5 * cos(2*x));
 }
 
-int main( int argc, char **argv ) {
-				int n= argc > 1 ? atoi( argv[1] ) : 100;
-				double a= argc > 2 ? atof( argv[2] ) : -3;
-				double b= argc > 3 ? atof( argv[3] ) : 2;
-				FILE *out= argc > 4 ? fopen( argv[4], "w" ) : stdout;
+int main(int argc, char **argv) {
+    int n = argc > 1 ? atoi(argv[1]) : 100;
+    double a = argc > 2 ? atof(argv[2]) : -3;
+    double b = argc > 3 ? atof(argv[3]) : 2;
+    FILE *out = argc > 4 ? fopen(argv[4], "w") : stdout;
 
-				srand( argc > 5 ? atoi(argv[5]) : time(NULL) );
+    srand(argc > 5 ? atoi(argv[5]) : time(NULL));
 
-				int i;
-				double dx = (b-a)/(n-1);
+    int i;
+    double dx = (b - a) / (n - 1);
 
-				for( i =0; i < n; i++ ) {
-								fprintf( out, "%g %g\n", a+i*dx, fun(a+i*dx) );
-				}
-				fclose( out );
+    for (i = 0; i < n; i++) {
+        fprintf(out, "%g %g\n", a + i * dx, fun(a + i * dx));
+    }
+    fclose(out);
 
-				return 0;
+    return 0;
 }
--- trygonometryczne.c
+++ trygonometryczne.c
@@ -2,53 +2,80 @@
 #include "gaus/matrix.h"
 #include <math.h>
 #include <stdlib.h>
-#include <stdio.h>
 #include "gaus/piv_ge_solver.h"
 
-#define PART_COUNT 1;
+#ifndef APPROX_BASE_SIZE
+#define APPROX_BASE_SIZE 4;
+#endif
 
 double diff_sin(double x, int n) {
-    return 1 * n * cos(n * x);
+    return sin(n * x);
 }
 
 double diff_cos(double x, int n) {
-    return -1 * n * sin(n * x);
+    return cos(n * x);
 }
 
 double *get_factors(matrix_t *);
 
 void make_spl(points_t *pts, spline_t *spl) {
     int i, j, k;
-    int parts = PART_COUNT;
+    int parts = APPROX_BASE_SIZE;
     int x_size = pts->n;
+
+    if (parts < x_size)
+        fprintf(stderr, "\tIlość punktów jest większa niż funkcji bazowych - wynik znacząco traci na dokładności");
+
     matrix_t *equations = make_matrix(parts, parts + 1);
 
-    //rows
+    /*zmienna k odpowiada za rząd - czyli dzięki k zmienia się: POCHODNA.*/
     for (k = 0; k < parts; k++) {
-        //parts - cols
-        for (j = 0; j < parts; j++) {
+        double (*derivative)(double, int);
+
+        /*n takie, że derivative=sin/cos(n*x)*/
+        int derN = k / 2 + 1;
+
+        /*jeśli k jest nieparzyste, to jesteśmy na sinusie (póki co - później dojdzie jeszcze liczba rzeczywista)*/
+        if (k % 2 == 0) derivative = &diff_sin;
+        else derivative = &diff_cos;
+
+        /*zmienna j odpowiada za kolumnę - czyli zmienia: FUNKCJĘ.*/
+        for (j = 0; j < parts + 1; j++) {
+            /*suma w kolumnie*/
             double sum = 0;
-            double (*func)(double, int);
+            double (*func)(double);
 
-            //x
-            for (i = 0; i < x_size; i++) {
-                int n = j / 2 + 1;
-                if (j % 2 == 0) func = &diff_sin;
-                else func = &diff_cos;
+            /*n takie, że func=sin/cos(n*x)*/
+            double funcN = j / 2 + 1;
 
-                sum += pts->x[i] * pts->x[i];
-//                sum += func(pts->x[j], n) * pts->x[i];
+            if (j % 2 == 0) func = &sin;
+            else func = &cos;
+
+            if (j < parts) {
+                //dodajemy pochodną razy wartość funkcji bazowej dla danego x
+                for (i = 0; i < x_size; i++) {
+                    double x = pts->x[i];
+
+                    sum += func(x * funcN)
+                           * derivative(x, derN);
+                }
             }
+            else {
+                //dodajemy pochodną dla x razy wartość y które znamy
+                printf("Pochodna: %s(%dx)\n", k % 2 ? "cos" : "sin", derN);
+                for (i = 0; i < x_size; i++) {
+                    double x = pts->x[i];
+                    double y = pts->y[i];
+
+                    sum += y * derivative(x, derN);
+                }
+            }
+
             put_entry_matrix(equations, k, j, sum);
         }
-
-        //this part is ok
-        double sumY = 0;
-        for (i = 0; i < x_size; i++) {
-            sumY += pts->y[i] * pts->x[i];
-        }
-        put_entry_matrix(equations, k, parts, sumY);
     }
+
+    write_matrix(equations, stdout);
 
     piv_ge_solver(equations);
 
@@ -72,10 +99,9 @@ int main(int argc, char *argv[]) {
     spline_t spl;
     pts.n = 0;
     spl.n = 0;
-    FILE *in = fopen("dane.lel", "r");
+    FILE *in = fopen("/Users/kubukoz/ClionProjects/lmp10/dane.lel", "r");
     read_pts_failed(in, &pts);
     make_spl(&pts, &spl);
-
 
     return 0;
 }
